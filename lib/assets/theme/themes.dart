f20fee1115128c5efcecf046e1502ab9393e0ce1
Davide Belvisi <davide@epublicsrl.it>
2022-03-18 16:50:35 +0100

    This is First Commit

---
+++ lib/assets/theme/themes.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '/assets/theme/constants.dart';
+
+class MyTheme {
+  static final lightTheme = ThemeData(
+      brightness: Brightness.light,
+      primaryColor: kPrimaryColor,
+      primaryColorDark: kPrimaryColor,
+      appBarTheme: AppBarTheme(systemOverlayStyle: SystemUiOverlayStyle.light)
+          .copyWith(color: kPrimaryColor),
+      scaffoldBackgroundColor: kWhite,
+      cardColor: kWhite,
+      toggleableActiveColor: kPrimaryColor,
+      accentColor: kPrimaryColor,
+      progressIndicatorTheme: ProgressIndicatorThemeData(color: kPrimaryColor)
+      // colorScheme: ColorScheme.fromSwatch().copyWith(secondary: kPrimaryColor),
+      );
+
+  static final darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    primaryColor: kGrey,
+    primaryColorDark: kPrimaryLightColor,
+    appBarTheme: AppBarTheme(systemOverlayStyle: SystemUiOverlayStyle.light)
+        .copyWith(color: kGrey),
+    scaffoldBackgroundColor: kBlack,
+    cardColor: kBlack,
+    toggleableActiveColor: kPrimaryLightColor,
+    accentColor: kWhite,
+    progressIndicatorTheme: ProgressIndicatorThemeData(color: kWhite),
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+        foregroundColor: kPrimaryLightColor, backgroundColor: kWhite),
+  );
+}
