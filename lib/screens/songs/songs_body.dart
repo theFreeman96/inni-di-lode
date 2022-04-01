586c43de19b7c256df7b157dff892df0eaecee8e
davide <davide.belvisi96@gmail.com>
2022-04-01 10:35:39 +0200

    Added first version of paging with CupertinoSlidingSegmentedControl

--- lib/screens/songs/songs_body.dart
+++ lib/screens/songs/songs_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 
 import '/theme/provider.dart';
@@ -16,13 +17,40 @@ class SongsBody extends StatefulWidget {
 class _SongsBodyState extends State<SongsBody> {
   TextEditingController editingController = TextEditingController();
   FocusNode myFocusNode = FocusNode();
-  final QueryCtr _query = QueryCtr();
+
+  final Map<int, Widget> children = const <int, Widget>{
+    0: Text('1'),
+    1: Text('2'),
+    2: Text('3'),
+    3: Text('4'),
+    4: Text('5'),
+    5: Text('6'),
+    6: Text('7'),
+  };
+
+  final List<Future<List?>> _query = [
+    QueryCtr().getSongsFrom1To100(),
+    QueryCtr().getSongsFrom101To200(),
+    QueryCtr().getSongsFrom201To300(),
+    QueryCtr().getSongsFrom301To400(),
+    QueryCtr().getSongsFrom401To500(),
+    QueryCtr().getSongsFrom501To600(),
+    QueryCtr().getSongsFrom601To700(),
+  ];
+
+  int currentSegment = 0;
+
+  void onValueChanged(newValue) {
+    setState(() {
+      currentSegment = newValue;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeProvider>(context);
     return Column(
-      children: [
+      children: <Widget>[
         Padding(
           padding: const EdgeInsets.all(kDefaultPadding),
           child: TextField(
@@ -59,32 +87,49 @@ class _SongsBodyState extends State<SongsBody> {
             ),
           ),
         ),
+        Padding(
+          padding: const EdgeInsets.only(bottom: kDefaultPadding),
+          child: CupertinoSlidingSegmentedControl<int>(
+            children: children,
+            onValueChanged: onValueChanged,
+            groupValue: currentSegment,
+            thumbColor:
+                themeProvider.isDarkMode ? kPrimaryLightColor : Colors.white,
+          ),
+        ),
         const Divider(height: 0.0),
-        FutureBuilder<List?>(
-          future: _query.getAllSongs(),
-          initialData: const [],
-          builder: (context, snapshot) {
-            return snapshot.hasData
-                ? Expanded(
-                    child: ListView.separated(
-                      physics: const ScrollPhysics(),
-                      shrinkWrap: true,
-                      itemCount: snapshot.data!.length,
-                      itemBuilder: (context, i) {
-                        return _buildRow(snapshot.data![i]);
-                      },
-                      separatorBuilder: (context, index) {
-                        return const Divider();
-                      },
-                    ),
-                  )
-                : const Padding(
-                    padding: EdgeInsets.all(kDefaultPadding * 2),
-                    child: Center(
-                      child: CircularProgressIndicator(),
-                    ),
-                  );
-          },
+        CupertinoUserInterfaceLevel(
+          data: CupertinoUserInterfaceLevelData.base,
+          child: Builder(
+            builder: (BuildContext context) {
+              return FutureBuilder<List?>(
+                future: _query[currentSegment],
+                initialData: const [],
+                builder: (context, snapshot) {
+                  return snapshot.hasData
+                      ? Expanded(
+                          child: ListView.separated(
+                            physics: const ScrollPhysics(),
+                            shrinkWrap: true,
+                            itemCount: snapshot.data!.length,
+                            itemBuilder: (context, i) {
+                              return _buildRow(snapshot.data![i]);
+                            },
+                            separatorBuilder: (context, index) {
+                              return const Divider();
+                            },
+                          ),
+                        )
+                      : const Padding(
+                          padding: EdgeInsets.all(kDefaultPadding * 2),
+                          child: Center(
+                            child: CircularProgressIndicator(),
+                          ),
+                        );
+                },
+              );
+            },
+          ),
         ),
       ],
     );
