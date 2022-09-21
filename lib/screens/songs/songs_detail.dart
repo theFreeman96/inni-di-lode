13d7c781decb1412ff6c3d3f108bf4e1c77a7890
Davide Belvisi <davide.belvisi96@gmail.com>
2022-09-21 17:38:03 +0200

    Import edit

--- lib/screens/songs/songs_detail.dart
+++ lib/screens/songs/songs_detail.dart
@@ -4,9 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart';
 
-import '/assets/data/models.dart';
 import '/theme/constants.dart';
 import '/theme/provider.dart';
+import '/assets/data/models.dart';
 import '/assets/data/queries.dart';
 
 import 'songs_pdf.dart';
