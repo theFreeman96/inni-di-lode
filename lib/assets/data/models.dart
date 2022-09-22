d0d70efb83a4701b10b48bcb470991ac60b0fa68
Davide Belvisi <davide.belvisi96@gmail.com>
2022-09-22 09:45:31 +0200

    Cleaned unnecessary models from db

--- lib/assets/data/models.dart
+++ lib/assets/data/models.dart
@@ -2,6 +2,7 @@ class Raccolta {
   late int songId;
   late String songTitle;
   late String songText;
+  late int isFav;
   late int macroId;
   late String macroName;
   late int catId;
@@ -11,6 +12,7 @@ class Raccolta {
     songId = obj['songId'];
     songTitle = obj['songTitle'];
     songText = obj['songText'];
+    isFav = obj['isFav'];
     macroId = obj['macroId'];
     macroName = obj['macroName'];
     catId = obj['catId'];
@@ -31,63 +33,5 @@ class Autori {
     songId = obj['songId'];
     songTitle = obj['songTitle'];
     songText = obj['songText'];
-  }
-}
-
-class Songs {
-  late int id;
-  late String title;
-  late String text;
-
-  Songs.fromMap(dynamic obj) {
-    id = obj['id'];
-    title = obj['title'];
-    text = obj['text'];
-  }
-}
-
-class Macrocategories {
-  late int id;
-  late String name;
-
-  Macrocategories.fromMap(dynamic obj) {
-    id = obj['id'];
-    name = obj['name'];
-  }
-}
-
-class Categories {
-  late int id;
-  late String name;
-  late int macro_id;
-
-  Categories.fromMap(dynamic obj) {
-    id = obj['id'];
-    name = obj['name'];
-    macro_id = obj['macro_id'];
-  }
-}
-
-class Authors {
-  late int id;
-  late String name;
-  late String surname;
-
-  Authors.fromMap(dynamic obj) {
-    id = obj['id'];
-    name = obj['name'];
-    surname = obj['surname'];
-  }
-}
-
-class Songs_Authors {
-  late int song_id;
-  late int aut_id;
-  late String song_title;
-
-  Songs_Authors.fromMap(dynamic obj) {
-    song_id = obj['song_id'];
-    aut_id = obj['aut_id'];
-    song_title = obj['song_title'];
   }
 }
