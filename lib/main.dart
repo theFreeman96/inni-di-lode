c6c52305802412094914fd358ad380c3a609617d
Davide Belvisi <davide.belvisi96@gmail.com>
2022-09-28 11:33:12 +0200

    Renamed themes file

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import "package:flutter/material.dart";
 import 'package:provider/provider.dart';
 
 import '/theme/theme_provider.dart';
-import '/theme/app_themes.dart';
+import '/theme/themes.dart';
 
 import '/screens/home.dart';
 
