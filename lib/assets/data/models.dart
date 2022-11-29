cc30542c0d5229b0c5d8f2baef2205571f6939e7
Davide Belvisi <davide.belvisi96@gmail.com>
2022-11-29 16:07:49 +0100

    Removed unnecessary model

--- lib/assets/data/models.dart
+++ lib/assets/data/models.dart
@@ -7,6 +7,8 @@ class Raccolta {
   late String macroName;
   late int catId;
   late String catName;
+  late int autId;
+  late String autName;
 
   Raccolta.fromMap(dynamic obj) {
     songId = obj['songId'];
@@ -17,21 +19,7 @@ class Raccolta {
     macroName = obj['macroName'];
     catId = obj['catId'];
     catName = obj['catName'];
-  }
-}
-
-class Autori {
-  late int autId;
-  late String autName;
-  late int songId;
-  late String songTitle;
-  late String songText;
-
-  Autori.fromMap(dynamic obj) {
     autId = obj['autId'];
     autName = obj['autName'];
-    songId = obj['songId'];
-    songTitle = obj['songTitle'];
-    songText = obj['songText'];
   }
 }
