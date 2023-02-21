db2ec63eef48b0509b1e4c35228fd573c32927aa
Davide Belvisi <davide.belvisi96gmail.com>
2023-02-21 23:25:28 +0100

    Refactored

--- lib/components/searchbar.dart
+++ lib/components/searchbar.dart
@@ -2,30 +2,26 @@ import 'package:flutter/material.dart';
 
 import '/utilities/constants.dart';
 
-Widget buildSearchBar(myFocusNode, runFilter, {required String from}) {
+Widget buildSearchBar(
+    {required FocusNode focusNode,
+    required Function filter,
+    required String label,
+    required String hint}) {
   return Padding(
     padding: const EdgeInsets.all(kDefaultPadding),
     child: TextField(
-      focusNode: myFocusNode,
+      focusNode: focusNode,
       autofocus: false,
       onChanged: (value) {
-        runFilter(value);
+        filter(value);
       },
       decoration: InputDecoration(
         prefixIcon: const Icon(
           Icons.search,
           color: kLightGrey,
         ),
-        labelText: from == 'Cantici' || from == 'Preferiti'
-            ? 'Cerca per numero, titolo o testo'
-            : from == 'Categorie'
-                ? 'Cerca una categoria'
-                : from == 'Autori'
-                    ? 'Cerca un autore'
-                    : 'Cerca',
-        hintText: from == 'Cantici' || from == 'Preferiti'
-            ? 'Cerca un cantico'
-            : 'Cerca',
+        labelText: label,
+        hintText: hint,
       ),
     ),
   );
