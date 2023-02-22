64ccfe9b8bbbb574c55ca8b033145189fdef2909
Davide Belvisi <davide.belvisi96@gmail.com>
2023-02-22 12:10:14 +0100

    Made header image required

--- lib/components/header.dart
+++ lib/components/header.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 
 import '/utilities/constants.dart';
 
-Widget buildHeader(headerImages) {
+Widget buildHeader({
+  required String image,
+}) {
   return Container(
     decoration: BoxDecoration(
       image: DecorationImage(
         fit: BoxFit.cover,
-        image: AssetImage(headerImages),
+        image: AssetImage(image),
         alignment: Alignment.center,
       ),
       color: kPrimaryColor,
