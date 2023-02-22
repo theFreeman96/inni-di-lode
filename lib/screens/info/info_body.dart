30e339d98d0af10e5042bfbd2bf9294349a02060
Davide Belvisi <davide.belvisi96@gmail.com>
2023-02-22 16:09:04 +0100

    Converted to StatelessWidget

--- lib/screens/info/info_body.dart
+++ lib/screens/info/info_body.dart
@@ -3,14 +3,9 @@ import 'package:url_launcher/link.dart';
 
 import '/utilities/constants.dart';
 
-class InfoBody extends StatefulWidget {
+class InfoBody extends StatelessWidget {
   const InfoBody({Key? key}) : super(key: key);
 
-  @override
-  State<InfoBody> createState() => _InfoBodyState();
-}
-
-class _InfoBodyState extends State<InfoBody> {
   @override
   Widget build(BuildContext context) {
     return ListView(
