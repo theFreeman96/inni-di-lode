acf0bfa8ebbd76057427de35d2f0f910607f8e53
Davide Belvisi <davide.belvisi96gmail.com>
2024-01-19 01:07:37 +0100

    song not found widget

--- lib/components/detail_list.dart
+++ lib/components/detail_list.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 import '/utilities/constants.dart';
-import '/utilities/theme_provider.dart';
-
-import '/screens/editor/editor.dart';
+import 'song_not_found.dart';
 
 class DetailList extends StatelessWidget {
   const DetailList({
@@ -24,7 +21,6 @@ class DetailList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final themeProvider = Provider.of<ThemeProvider>(context);
     return FutureBuilder<List?>(
       future: future,
       initialData: const [],
@@ -52,39 +48,7 @@ class DetailList extends StatelessWidget {
               )
             : Center(
                 child: condition
-                    ? Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Text('Nessun cantico trovato',
-                              style: TextStyle(fontSize: 20.0)),
-                          TextButton.icon(
-                            onPressed: () {
-                              FocusScope.of(context).unfocus();
-                              Navigator.pop(context);
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) {
-                                    return const Editor();
-                                  },
-                                ),
-                              );
-                            },
-                            icon: Icon(Icons.add_circle,
-                                color: themeProvider.isDarkMode
-                                    ? kPrimaryLightColor
-                                    : kPrimaryColor),
-                            label: Text(
-                              'Aggiungi cantico',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: themeProvider.isDarkMode
-                                      ? kWhite
-                                      : kBlack),
-                            ),
-                          ),
-                        ],
-                      )
+                    ? const SongNotFound()
                     : const CircularProgressIndicator(),
               );
       },
